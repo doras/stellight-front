d82ccc475e02f69557dc3380cbda23e86b3aa68b
doras <shoon0113@gmail.com>
2023-02-27 05:58:33 +0900

    Config: axios base url with env file

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000' // Set your API endpoint here
+  baseURL: import.meta.env.VITE_BASE_URL // Set your API endpoint here
 });
 
 export default {
